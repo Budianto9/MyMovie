218a63adf1bcafe922ede32ed8d859ad38a5e897
budiantosuhanda <budi0586@gmail.com>
2021-11-21 10:53:56 +0700

    https://github.com/Budianto1992/MyMovie.git

--- core/src/main/java/com/example/mymovie/core/data/remote/response/BaseResponse.kt
+++ core/src/main/java/com/example/mymovie/core/data/remote/response/BaseResponse.kt
@@ -1,4 +1,18 @@
-package com.example.mymovie.core.data.remote.response
+package com.example.mymovie.core.data.remote.datasource
 
-class BaseResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class BaseResponse(
+
+	@field:SerializedName("page")
+	val page: Int? = null,
+
+	@field:SerializedName("total_pages")
+	val totalPages: Int? = null,
+
+	@field:SerializedName("results")
+	val results: List<Any?>? = null,
+
+	@field:SerializedName("total_results")
+	val totalResults: Int? = null
+)
