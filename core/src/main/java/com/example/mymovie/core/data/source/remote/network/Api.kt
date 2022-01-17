0206ceafd2b4807c22507543e1870612791ad694
IT24 <budi0586@gmail.com>
2022-01-17 14:18:31 +0700

    -put secret api-key from themoviedb

--- core/src/main/java/com/example/mymovie/core/data/source/remote/network/Api.kt
+++ core/src/main/java/com/example/mymovie/core/data/source/remote/network/Api.kt
@@ -1,6 +1,6 @@
 package com.example.mymovie.core.data.source.remote.network
 
-import com.example.mymovie.core.data.source.remote.response.base.BaseResponse
+import com.example.mymovie.core.data.source.remote.response.MovieListResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,7 +8,8 @@ import retrofit2.http.Query
 interface Api {
     @GET("3/discover/movie?sort_by=vote_count.desc")
     suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String,
         @Query("page") page: Int
-    ) : Response<BaseResponse>
+    ) : Response<MovieListResponse>
 
 }
