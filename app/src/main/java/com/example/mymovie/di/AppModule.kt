49f5dc1063651399a82f9d926646eae67dd9d2ad
IT24 <budi0586@gmail.com>
2022-01-19 13:12:31 +0700

    fix app module

--- app/src/main/java/com/example/mymovie/di/AppModule.kt
+++ app/src/main/java/com/example/mymovie/di/AppModule.kt
@@ -3,7 +3,7 @@ package com.example.mymovie.di
 import com.example.mymovie.core.domain.usecase.MovieInteractor
 import com.example.mymovie.core.domain.usecase.MovieUseCase
 import com.example.mymovie.main.MainViewModel
-import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 
