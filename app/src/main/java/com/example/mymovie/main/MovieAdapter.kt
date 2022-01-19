1078699f532bbaee8d1cad977460835c411f7f7a
IT24 <budi0586@gmail.com>
2022-01-19 13:14:10 +0700

    fix adapter -add binding view

---
+++ app/src/main/java/com/example/mymovie/main/MovieAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.mymovie.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymovie.core.data.source.remote.response.MovieResponse
+import com.example.mymovie.databinding.ListItemMovieBinding
+
+class MovieAdapter :
+    PagingDataAdapter<MovieResponse, MovieAdapter.MovieViewHolder>(movieDiffCallback){
+
+    companion object {
+        val movieDiffCallback = object : DiffUtil.ItemCallback<MovieResponse>() {
+            override fun areItemsTheSame(oldItem: MovieResponse, newItem: MovieResponse): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: MovieResponse, newItem: MovieResponse): Boolean {
+                return oldItem == newItem
+            }
+        }
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = ListItemMovieBinding.inflate(inflater, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val data = getItem(position)
+        if (data != null){
+            holder.bindTo(data)
+        }
+    }
+
+
+    inner class MovieViewHolder(private val binding: ListItemMovieBinding)
+        : RecyclerView.ViewHolder(binding.root){
+            fun bindTo(item: MovieResponse){
+                binding.item = item
+                binding.executePendingBindings()
+            }
+        }
+}
---
+++ app/src/main/java/com/example/mymovie/util/ImageBindingAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.mymovie.util
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.example.mymovie.util.constant.Constant
+
+object ImageBindingAdapter {
+
+    @JvmStatic
+    @BindingAdapter("imgUrl")
+    fun setImageUrl(view: ImageView, url: String?) {
+
+        if (url == null) return
+
+        Glide.with(view.context)
+            .load(Constant.IMAGE_URL + url)
+            .into(view)
+    }
+}
