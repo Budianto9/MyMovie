8b4a491c947a6664dc9515b8bb939bec677d09d8
iqbal <iqbalvzr@gmail.com>
2022-01-20 16:36:36 +0700

    Nambah design detail movie

---
+++ app/src/main/java/com/example/mymovie/DetailMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mymovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_movie)
+    }
+}
