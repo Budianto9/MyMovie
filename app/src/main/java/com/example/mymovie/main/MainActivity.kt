a56818fc80817e321b02b52d3fc2b8475a59e0b0
IT24 <budi0586@gmail.com>
2022-01-20 13:38:54 +0700

    generated code

--- app/src/main/java/com/example/mymovie/main/MainActivity.kt
+++ app/src/main/java/com/example/mymovie/main/MainActivity.kt
@@ -1,17 +1,51 @@
 package com.example.mymovie.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mymovie.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var movieAdapter: MovieAdapter
 
-    private val viewModel: MainViewModel by viewModel()
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initUi()
+        observeView()
+    }
+
+    private fun initUi(){
+        movieAdapter = MovieAdapter()
+        binding.rvMovie.layoutManager = LinearLayoutManager(this)
+        binding.rvMovie.setHasFixedSize(true)
+        binding.rvMovie.adapter = movieAdapter
+
+//        lifecycleScope.launchWhenCreated {
+//            movieAdapter.loadStateFlow.collect { state->
+//                when{
+//                    state.source
+//                }
+//            }
+//        }
+
+    }
+
+    private fun observeView(){
+        lifecycleScope.launchWhenCreated {
+            viewModel.getMoviePopular().distinctUntilChanged().collectLatest {
+                movieAdapter.submitData(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mymovie/main/MainViewModel.kt
+++ app/src/main/java/com/example/mymovie/main/MainViewModel.kt
@@ -2,12 +2,18 @@ package com.example.mymovie.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.mymovie.BuildConfig
+import com.example.mymovie.core.data.source.remote.response.MovieResponse
 import com.example.mymovie.core.domain.usecase.MovieUseCase
+import kotlinx.coroutines.flow.Flow
 
-class MainViewModel(movieUseCase: MovieUseCase) : ViewModel() {
+class MainViewModel(private val movieUseCase: MovieUseCase) : ViewModel() {
 
     private val apiKey = BuildConfig.API_KEY
 
-    val movie = movieUseCase.getMoviePopularPage(apiKey).asLiveData()
+    fun getMoviePopular() : Flow<PagingData<MovieResponse>> =
+        movieUseCase.getMoviePopularPage(apiKey).cachedIn(viewModelScope)
 }
