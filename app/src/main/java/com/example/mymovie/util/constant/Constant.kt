9d30bd7a68d30f2840608eb01f60bfff6f1aa396
IT24 <budi0586@gmail.com>
2022-01-25 11:20:00 +0700

    progress layout

--- app/src/main/java/com/example/mymovie/util/constant/Constant.kt
+++ app/src/main/java/com/example/mymovie/util/constant/Constant.kt
@@ -3,6 +3,6 @@ package com.example.mymovie.util.constant
 class Constant {
 
     companion object{
-        const val IMAGE_URL = "http://image.tmdb.org/t/p/w500/"
+        const val IMAGE_URL = "http://image.tmdb.org/t/p/w500"
     }
 }
