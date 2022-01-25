ad6de499e91dfbc65da91a6391c1d75d1e6b2c04
IT24 <budi0586@gmail.com>
2022-01-25 11:18:54 +0700

    cuma gara2 salah import aplikasi ga jalan ternyata disini penyakit nya

--- app/src/main/java/com/example/mymovie/main/MainActivity.kt
+++ app/src/main/java/com/example/mymovie/main/MainActivity.kt
@@ -1,42 +1,65 @@
 package com.example.mymovie.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mymovie.databinding.ActivityMainBinding
+import com.kennyc.view.MultiStateView
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var movieAdapter: MovieAdapter
 
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initUi()
+        observeView()
     }
 
-//    private fun initUi(){
-//        movieAdapter = MovieAdapter()
-//        binding.rvMovie.layoutManager = LinearLayoutManager(this)
-//        binding.rvMovie.setHasFixedSize(true)
-//        binding.rvMovie.adapter = movieAdapter
-//
-//
-//    }
-//
-//    private fun observeView(){
-//        lifecycleScope.launchWhenCreated {
-//            viewModel.getMoviePopular().distinctUntilChanged().collectLatest {
-//                movieAdapter.submitData(it)
-//            }
-//        }
-//    }
+    private fun initUi(){
+        movieAdapter = MovieAdapter()
+        binding.rvMovie.layoutManager = LinearLayoutManager(this)
+        binding.rvMovie.setHasFixedSize(true)
+        binding.rvMovie.adapter = movieAdapter
+
+
+        lifecycleScope.launchWhenCreated {
+            movieAdapter.loadStateFlow.collectLatest { state ->
+                when {
+                    state.source.refresh is LoadState.Loading -> {
+                        binding.layoutState.viewState = MultiStateView.ViewState.LOADING
+                        binding.proggerBarShimmer.visibility = View.VISIBLE
+                    }
+                    movieAdapter.itemCount < 1 -> {
+                        binding.layoutState.viewState = MultiStateView.ViewState.ERROR
+                    }
+                    else -> {
+                        binding.layoutState.viewState = MultiStateView.ViewState.CONTENT
+                        binding.proggerBarShimmer.visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun observeView(){
+        lifecycleScope.launchWhenCreated {
+            viewModel.getMoviePopular().distinctUntilChanged().collectLatest {
+                movieAdapter.submitData(it)
+            }
+        }
+    }
 }
