6ea827285ae8b3a1825eb27fb7e60c80bc6b7e88
IT24 <budi0586@gmail.com>
2022-01-25 11:16:32 +0700

    rename endpoint tmdb

--- core/src/main/java/com/example/mymovie/core/data/source/remote/network/Api.kt
+++ core/src/main/java/com/example/mymovie/core/data/source/remote/network/Api.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Api {
-    @GET("3/discover/movie?sort_by=vote_count.desc")
+    @GET("movie/now_playing")
     suspend fun getPopularMovies(
         @Query("api_key") apiKey: String,
         @Query("page") page: Int
